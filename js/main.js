1ecc011d3b2fa982771ce60b2fb387aee3558769
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-04-04 21:07:06 -0700

    first

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-function todolist(){
-    var item = document.getElementById("todoinput").value
-    var text = document.createTextNode(item)
-    var newItem = document.createElement("li")
-    newItem.appendChild(text)
-    document.getElementById("todolist").appendChild(newItem)
+function getComment() {
+	var name = document.getElementById('name').value;
+	var post = document.getElementById('commentText').value;
+	postComment(name, post);
+	addPicture(name);
+
 }
+
+function postComment(name, post) {
+	var h2 = document.createElement('h2');
+	var h3 = document.createElement('h3');
+	var d = new Date();
+	h2.textContent = d;
+	h3.textContent = name;
+	var userName = document.getElementById('postComment');
+	userName.appendChild(h2);
+	userName.appendChild(h3);
+	var p = document.createElement('p');
+	p.textContent = post;
+	userName.appendChild(p);
+	p.className = "text";
+}	
